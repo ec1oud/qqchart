d6aee1b560160a628867c6e07072062e38fd4f4d
Shawn Rutledge <s@ecloud.org>
2016-01-13 08:12:03 +0100

    LineGraphModel: add LargestTriangleThreeBuckets resampling method
    This can be used to keep sample frequency <= horizontal pixel resolution,
    which helps the graph look cleaner while preserving the perceptual shape
    of the signal.

--- linegraphmodel.cpp
+++ linegraphmodel.cpp
@@ -5,44 +5,82 @@ LineGraphModel::LineGraphModel(QObject *parent) : QObject(parent)
 
 }
 
+double LineGraphModel::triangleArea(
+        const LineGraphModel::TimeValue &a, const LineGraphModel::TimeValue &b, const LineGraphModel::TimeValue &c)
+{
+    // www.mathopenref.com/coordtrianglearea.html
+    return qAbs( (a.time * (b.value - c.value) +
+                  b.time * (c.value - a.value) +
+                  c.time * (a.value - b.value)) / 2);
+}
+
+LineGraphModel::TimeValue LineGraphModel::largestTriangle(const TimeValue &previous, const QVector<TimeValue> &current, const TimeValue &next)
+{
+    if (current.count() == 1)
+        return current.first();
+
+    int chosen = 0;
+    double chosenArea = triangleArea(previous, current.first(), next);
+    for (int i = 1; i < current.size(); ++i) {
+        if (triangleArea(previous, current[i], next) > chosenArea)
+            chosen = i;
+    }
+
+    return current[chosen];
+}
+
+LineGraphModel::TimeValue LineGraphModel::average(const QVector<TimeValue> &tvv)
+{
+    TimeValue ret = {0, 0};
+    foreach (const TimeValue &tv, tvv) {
+        ret.time += tv.time;
+        ret.value += tv.value;
+    }
+    ret.time /= tvv.count();
+    ret.value /= tvv.count();
+    return ret;
+}
+
+
+LineGraphModel::TimeValue LineGraphModel::endVertex(int fromLast)
+{
+    int i = (m_vertices.count() / LineNode::verticesPerSample - fromLast) * LineNode::verticesPerSample;
+    TimeValue ret;
+    ret.time = m_vertices[i].x /* + m_timeOffset */;
+    ret.value = m_vertices[i].y;
+    return ret;
+}
+
 void LineGraphModel::appendSample(qreal value, qint64 timestamp)
 {
-//    qDebug() << m_label << timestamp << value << "already have samples:" << m_vertices.size();
     // make time (x) values smaller; TODO why does this even matter? loss of precision?
     float time = (timestamp - m_timeOffset) / 1000.0;
-    if (value > m_maxSampleValue) {
-        m_maxSampleValue = value;
-        emit maxSampleValueChanged();
+    TimeValue tv { time, value };
+    // On currentBucket overflow:
+    // finalize the vertex from m_previousBucket
+    // current becomes previous
+    bool modify = !m_currentBucket.isEmpty();
+    if (m_downsampleMethod == LargestTriangleThreeBuckets) {
+        if (!m_currentBucket.isEmpty() && time > m_currentBucket.first().time + m_downsampleBucketInterval) {
+            if (m_vertices.count() > LineNode::verticesPerSample * 2) {
+                TimeValue previousVtx = largestTriangle(endVertex(2), m_previousBucket, tv);
+                modifyEndVertices(previousVtx.time, previousVtx.value, 1);
+            }
+            m_previousBucket = m_currentBucket;
+            m_currentBucket.clear();
+            TimeValue previousVtx = average(m_previousBucket);
+            modifyEndVertices(previousVtx.time, previousVtx.value);
+            modify = false;
+        }
+        m_currentBucket.append(tv);
+//        qDebug() << "buckets" << m_previousBucket.count() << m_currentBucket.count();
+    } else {
+        modify = false;
     }
-    if (value < m_minSampleValue) {
-        m_minSampleValue = value;
-        emit minSampleValueChanged();
-    }
-    int i = m_vertices.size() - LineNode::verticesPerSample;
-    if (i >= 0) {
-        m_vertices[i].nextX = time;
-        m_vertices[i++].nextY = value;
-        m_vertices[i].nextX = time;
-        m_vertices[i++].nextY = value;
-        m_vertices[i].nextX = time;
-        m_vertices[i++].nextY = value;
-        m_vertices[i].nextX = time;
-        m_vertices[i++].nextY = value;
-    }
-    i = m_vertices.size();
-    m_vertices.resize(i + LineNode::verticesPerSample);
-    float tp = i > 0 ? m_vertices[i - 1].x : time;
-    float samplePrev = i > 0 ? m_vertices[i - 1].y : value;
-    m_vertices[i++].set(0, -1, time, value, tp, samplePrev, time + 0.01, value);
-    m_vertices[i++].set(1,  1, time, value, tp, samplePrev, time + 0.01, value);
-    m_vertices[i++].set(2, -1, time, value, tp, samplePrev, time + 0.01, value);
-    m_vertices[i++].set(3,  1, time, value, tp, samplePrev, time + 0.01, value);
-    Q_ASSERT(i == m_vertices.size());
-
-    qint64 earliestTimeAllowed = m_vertices.last().x - m_timeSpan;
-    while (m_vertices.size() > 2 && m_vertices[1].x < earliestTimeAllowed)
-        m_vertices.remove(0, 4);
-    emit samplesChanged();
+    if (modify)
+        modifyEndVertices(time, value);
+    else
+        appendVertices(time, value);
 }
 
 int LineGraphModel::sampleIndexNearest(qint64 time)
@@ -102,6 +140,24 @@ void LineGraphModel::setTimeSpan(int timeSpan)
     emit timeSpanChanged();
 }
 
+void LineGraphModel::setDownsampleInterval(qreal downsampleInterval)
+{
+    if (m_downsampleBucketInterval == downsampleInterval)
+        return;
+
+    m_downsampleBucketInterval = downsampleInterval;
+    emit downsampleIntervalChanged();
+}
+
+void LineGraphModel::setDownsampleMethod(LineGraphModel::DownsampleMethod downsampleMethod)
+{
+    if (m_downsampleMethod == downsampleMethod)
+        return;
+
+    m_downsampleMethod = downsampleMethod;
+    emit downsampleMethodChanged();
+}
+
 qreal LineGraphModel::currentValue() const
 {
     if (m_vertices.length())
@@ -131,4 +187,84 @@ void LineGraphModel::setMaxValue(qreal maxValue)
 const QVector<LineNode::LineVertex> *LineGraphModel::vertices()
 {
     return &m_vertices;
+}
+
+void LineGraphModel::appendVertices(qreal time, qreal value)
+{
+//    qDebug() << m_label << time << value << "already have samples:" << m_vertices.size();
+    if (value > m_maxSampleValue) {
+        m_maxSampleValue = value;
+        emit maxSampleValueChanged();
+    }
+    if (value < m_minSampleValue) {
+        m_minSampleValue = value;
+        emit minSampleValueChanged();
+    }
+    int i = m_vertices.size() - LineNode::verticesPerSample;
+    if (i >= 0) {
+        m_vertices[i].nextX = time;
+        m_vertices[i++].nextY = value;
+        m_vertices[i].nextX = time;
+        m_vertices[i++].nextY = value;
+        m_vertices[i].nextX = time;
+        m_vertices[i++].nextY = value;
+        m_vertices[i].nextX = time;
+        m_vertices[i++].nextY = value;
+    }
+    i = m_vertices.size();
+    m_vertices.resize(i + LineNode::verticesPerSample);
+    float tp = i > 0 ? m_vertices[i - 1].x : time;
+    float samplePrev = i > 0 ? m_vertices[i - 1].y : value;
+    m_vertices[i++].set(0, -1, time, value, tp, samplePrev, time + 0.01, value);
+    m_vertices[i++].set(1,  1, time, value, tp, samplePrev, time + 0.01, value);
+    m_vertices[i++].set(2, -1, time, value, tp, samplePrev, time + 0.01, value);
+    m_vertices[i++].set(3,  1, time, value, tp, samplePrev, time + 0.01, value);
+    Q_ASSERT(i == m_vertices.size());
+
+    qint64 earliestTimeAllowed = m_vertices.last().x - m_timeSpan;
+    while (m_vertices.size() > 2 && m_vertices[1].x < earliestTimeAllowed)
+        m_vertices.remove(0, 4);
+    emit samplesChanged();
+}
+
+void LineGraphModel::modifyEndVertices(qreal time, qreal value, int fromLast)
+{
+//    qDebug() << m_label << time << value << "already have samples:" << m_vertices.size();
+    int i = (m_vertices.length() / LineNode::verticesPerSample - fromLast - 1) * LineNode::verticesPerSample;
+    Q_ASSERT(i >= 0);
+    qreal tp = time - 0.01;
+    qreal samplePrev = value;
+    if (i >= LineNode::verticesPerSample) {
+        int j = i - LineNode::verticesPerSample;
+        tp = m_vertices[j].x;
+        samplePrev = m_vertices[j].y;
+        m_vertices[j].nextX = time;
+        m_vertices[j++].nextY = value;
+        m_vertices[j].nextX = time;
+        m_vertices[j++].nextY = value;
+        m_vertices[j].nextX = time;
+        m_vertices[j++].nextY = value;
+        m_vertices[j].nextX = time;
+        m_vertices[j++].nextY = value;
+    }
+    qreal tn = time + 0.001;
+    qreal sampleNext = value;
+    if (fromLast > 0) {
+        int j = i + LineNode::verticesPerSample;
+        tn = m_vertices[j].x;
+        sampleNext = m_vertices[j].y;
+        m_vertices[j].prevX = time;
+        m_vertices[j++].prevY = value;
+        m_vertices[j].prevX = time;
+        m_vertices[j++].prevY = value;
+        m_vertices[j].prevX = time;
+        m_vertices[j++].prevY = value;
+        m_vertices[j].prevX = time;
+        m_vertices[j++].prevY = value;
+    }
+    m_vertices[i++].set(0, -1, time, value, tp, samplePrev, tn, sampleNext);
+    m_vertices[i++].set(1,  1, time, value, tp, samplePrev, tn, sampleNext);
+    m_vertices[i++].set(2, -1, time, value, tp, samplePrev, tn, sampleNext);
+    m_vertices[i++].set(3,  1, time, value, tp, samplePrev, tn, sampleNext);
+    emit samplesChanged();
 }
--- linegraphmodel.h
+++ linegraphmodel.h
@@ -9,6 +9,8 @@ class LineGraphModel : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int timeSpan READ timeSpan WRITE setTimeSpan NOTIFY timeSpanChanged)
+    Q_PROPERTY(qreal downsampleInterval READ downsampleInterval WRITE setDownsampleInterval NOTIFY downsampleIntervalChanged)
+    Q_PROPERTY(DownsampleMethod downsampleMethod READ downsampleMethod WRITE setDownsampleMethod NOTIFY downsampleMethodChanged)
     Q_PROPERTY(qreal currentValue READ currentValue NOTIFY samplesChanged)
     Q_PROPERTY(qreal minValue READ minValue WRITE setMinValue NOTIFY minValueChanged)
     Q_PROPERTY(qreal maxValue READ maxValue WRITE setMaxValue NOTIFY maxValueChanged)
@@ -19,10 +21,18 @@ class LineGraphModel : public QObject
     Q_PROPERTY(QString label READ label WRITE setLabel NOTIFY labelChanged)
 
 public:
+    enum DownsampleMethod { NoDownsample = 0, LargestTriangleThreeBuckets };
+
     explicit LineGraphModel(QObject *parent = 0);
 
     int timeSpan() const { return m_timeSpan; }
     void setTimeSpan(int timeSpan);
+
+    qreal downsampleInterval() const { return m_downsampleBucketInterval; }
+    void setDownsampleInterval(qreal downsampleInterval);
+
+    DownsampleMethod downsampleMethod() const { return m_downsampleMethod; }
+    void setDownsampleMethod(DownsampleMethod downsampleMethod);
 
     qreal currentValue() const;
 
@@ -50,6 +60,8 @@ public:
 
 signals:
     void timeSpanChanged();
+    void downsampleIntervalChanged();
+    void downsampleMethodChanged();
     void samplesChanged();
     void minSampleValueChanged();
     void maxSampleValueChanged();
@@ -65,11 +77,27 @@ public slots:
     void removeFirstSample();
 
 protected:
-    virtual const QVector<LineNode::LineVertex> *vertices();
+    struct TimeValue {
+        qreal time;
+        qreal value;
+    };
 
 protected:
+    virtual const QVector<LineNode::LineVertex> *vertices();
+    void appendVertices(qreal time, qreal value);
+    void modifyEndVertices(qreal time, qreal value, int fromLast = 0);
+    TimeValue endVertex(int fromLast = 0);
+    double triangleArea(const LineGraphModel::TimeValue &a, const LineGraphModel::TimeValue &b, const LineGraphModel::TimeValue &c);
+    LineGraphModel::TimeValue largestTriangle(const TimeValue &previous, const QVector<TimeValue> &current, const TimeValue &next);
+    LineGraphModel::TimeValue average(const QVector<TimeValue> &tvv);
+
+protected:
+    QVector<TimeValue> m_currentBucket;
+    QVector<TimeValue> m_previousBucket;
     QVector<LineNode::LineVertex> m_vertices;
     int m_timeSpan = 1000; // in seconds
+    DownsampleMethod m_downsampleMethod = LargestTriangleThreeBuckets;
+    qreal m_downsampleBucketInterval = 0; // time in seconds: duration of one bucket
     qreal m_minValue = 0;
     qreal m_maxValue = 1;
     qreal m_normalMinValue = 0;
